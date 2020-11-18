d2ec83997ff92dbde49358565198d0d9d1af9bda
Nigel <nigel.cheung@freecomm.com>
2020-11-18 18:17:13 +0800

    add Swift Package Manager installation support

---
+++ Package.swift
@@ -0,0 +1,22 @@
+// swift-tools-version:5.2
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+import PackageDescription
+
+let package = Package(
+    name: "SpreadsheetView",
+    platforms: [
+        .iOS(.v9)
+    ],
+    products: [
+        .library(name: "SpreadsheetView", targets: ["SpreadsheetView"])
+    ],
+    dependencies: [
+
+    ],
+    targets: [
+        .target(name: "SpreadsheetView", dependencies: []),
+    ],
+    swiftLanguageVersions: [
+        .v5
+    ]
+)
